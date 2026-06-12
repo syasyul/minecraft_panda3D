936b5870a6c6491abaea640a80b74ff543abb055
syasyasyambal <maghfiraaisyah039@gmail.com>
2026-06-12 16:09:38 +0700

    Update game.py

--- game.py
+++ game.py
@@ -1,11 +1,12 @@
 from direct.showbase.ShowBase import ShowBase
 from mapmanager import MapManager
-
+from hero import Hero
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
         self.land = MapManager()
-        self.land.loadLand("land.txt")
+        x, y = self.land.loadLand("land.txt")
+        self.hero = Hero((x // 2, y // 2, 2), self.land)
         base.camLens.setFov(90)
 
 game = Game()
